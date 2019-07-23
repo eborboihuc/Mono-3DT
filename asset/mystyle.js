936361a95f106848188de7b40784285469e4736a
eborboihuc <eborboihuc@gmail.com>
2019-07-23 23:57:36 +0800

    Remove title modification js script

--- asset/mystyle.js
+++ asset/mystyle.js
@@ -1,35 +1,3 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  
-  // Change title dynamically
-  if (n % 2) {document.title = "Joint Monocular 3D Detection and Tracking";}
-  else {document.title = "Tell Me Where to Look: Investigating Ways for Assisting Focus in 360° Video";}
-  
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-
 
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
